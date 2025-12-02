5d1c47455b115215d8ac46c98e6578e9f2f25e3d
Enescu <adelin_enescu@yahoo.com>
2025-12-02 15:39:16 +0200

    created books db

--- db.py
+++ db.py
@@ -1,4 +1,31 @@
 import sqlite3
+from datetime import datetime
 
-con = sqlite3.connect("db.py")
+DB_PATH = "books.db"
+
+con = sqlite3.connect(DB_PATH)
 cursor = con.cursor()
+
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS books (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        author TEXT,
+        description TEXT,
+        categories TEXT,
+        page_count INTEGER,
+        date_published TEXT,
+        google_id TEXT,
+        isbn TEXT,
+        created_at TEXT
+    );
+    """
+)
+cursor.execute("""
+    DELETE FROM books 
+"""
+)
+con.commit()
+res = cursor.execute("SELECT author FROM books")
+print(res.fetchone())
