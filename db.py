d53e497dc152fe6a95c178e76f1a433485e916f8
Enescu <adelin_enescu@yahoo.com>
2025-12-02 16:00:47 +0200

    added add book to db function

--- db.py
+++ db.py
@@ -1,14 +1,20 @@
 import sqlite3
 from datetime import datetime
 
+
 DB_PATH = "books.db"
+now = datetime.now()
+date_time = now.strftime(r"%m/%d/%Y %H:%M:%S")
 
-con = sqlite3.connect(DB_PATH)
-cursor = con.cursor()
+def get_connection():
+    return sqlite3.connect(DB_PATH)
 
-cursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS books (
+
+def init_db():
+    with get_connection() as conn:
+        conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS books (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT NOT NULL,
         author TEXT,
@@ -19,13 +25,46 @@ cursor.execute(
         google_id TEXT,
         isbn TEXT,
         created_at TEXT
-    );
-    """
-)
-cursor.execute("""
-    DELETE FROM books 
-"""
-)
-con.commit()
-res = cursor.execute("SELECT author FROM books")
-print(res.fetchone())
+        );
+        """
+        )
+    conn.commit()
+
+
+def add_book(
+    title: str,
+    author: str,
+    description: str,
+    categories: str,
+    page_count: int,
+    date_published: str,
+    google_id: str,
+    isbn: str,
+):
+    with get_connection() as conn:
+        conn.execute(
+            """
+            INSERT INTO books 
+            (title, author, description, categories, page_count,
+            date_published, google_id, isbn, created_at)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+            (title, author, description, categories, page_count, date_published,
+            google_id, isbn, date_time)
+        )
+        conn.commit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
