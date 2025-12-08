87fb3487ed57b963962ccd59a44a86f525aae940
Enescu <adelin_enescu@yahoo.com>
2025-12-08 16:33:47 +0200

    refactor: add from_metadata classmethod for easier  Bookout conversion

--- src/schemas.py
+++ src/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from models import BookWithMetadata
 
 class BookOut(BaseModel):
     sql_index: int
@@ -12,3 +13,17 @@ class BookOut(BaseModel):
     isbn: str | None 
     created_at: str
 
+    @classmethod
+    def from_metadata(cls, book: BookWithMetadata) -> BookOut:
+        return cls(
+            sql_index = book.sql_index,
+            title = book.raw_book.title,
+            author = book.raw_book.author,
+            description = book.raw_book.description,
+            categories = book.raw_book.categories,
+            page_count = book.raw_book.page_count,
+            date_published = book.raw_book.date_published,
+            book_id = book.raw_book.book_id,
+            isbn = book.raw_book.isbn,
+            created_at = book.created_at,
+        )
