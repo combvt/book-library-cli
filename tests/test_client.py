3124eaeae3e4c4b1e6774bb6a2db4bd30754b841
Enescu <adelin_enescu@yahoo.com>
2025-12-14 15:16:20 +0200

    test: add mock client asserting empty list

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,10 +1,31 @@
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).resolve().parents[1]))
-
 from src.google_api_client import GoogleBooksClient
-from src.config import API_KEY
+from src.models import Book
+import pytest
+
+
+def test_search_for_no_results(monkeypatch):
+    class MockResponse:
+        def raise_for_status(self):
+            pass
+
+        def json(self):
+            return {"items": []}
+
+    def mock_get(*args, **kwargs):
+        return MockResponse()
+
+    monkeypatch.setattr("src.google_api_client.requests.get", mock_get)
+
+    client = GoogleBooksClient(api_key="fake_key")
+    items = client.search_books("dsakjdawdjkwabdwa")
+
+    assert items == []
+
+
+def test_search_one_book(monkeypatch):
+    class MockResponse:
+        def raise_for_status(self):
+            pass
 
 
 def test_search_for_no_results():
