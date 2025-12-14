5aa4092528a5cc121ea339fc37f8e63b681732ec
Enescu <adelin_enescu@yahoo.com>
2025-12-14 15:16:56 +0200

    test: add mock client asserting 1 item

--- tests/test_client.py
+++ tests/test_client.py
@@ -27,8 +27,27 @@ def test_search_one_book(monkeypatch):
         def raise_for_status(self):
             pass
 
+        def json(self):
+            return {
+                "items": [{
+                    "id": "testds21",
+                    "volumeInfo": {
+                        "title": "test123",
+                        "pageCount": 25,
+                    }
+                }]
+            }
+    
+    def mock_get(*args, **kwargs):
+        return MockResponse()
+    
+    monkeypatch.setattr("src.google_api_client.requests.get", mock_get)
 
-def test_search_for_no_results():
-    client = GoogleBooksClient(API_KEY)
+    client = GoogleBooksClient(api_key="fake_key")
+    items = client.search_books("harry")
 
-    assert client.search_books("dsaijdnaskj435kjnds") == []
+    assert len(items) == 1
+    assert isinstance(items[0], Book)
+    assert items[0].title == "test123"
+    assert items[0].page_count == 25
+    assert items[0].book_id == "testds21"
