d6a57066608804041becf9baecfcb6903ce0c2f1
Enescu <adelin_enescu@yahoo.com>
2025-12-14 17:10:55 +0200

    test: override DB connection to run API tests on temp database

--- tests/test_sql_storage.py
+++ tests/test_sql_storage.py
@@ -1,9 +1,22 @@
 from src.storage.sql_storage import SqlLibraryStorage
 from src.models import Book
+import src.db
+import sqlite3
+import src.storage.sql_storage
 
 
-def test_get_stats_empty_library():
-    library = SqlLibraryStorage("test_sql.db")
+def test_get_stats_empty_library(monkeypatch, tmp_path):
+    test_db_path = tmp_path / "test.db"
+
+    def fake_get_connection():
+        return sqlite3.connect(test_db_path)
+    
+    monkeypatch.setattr(src.db, "get_connection", fake_get_connection)
+    monkeypatch.setattr(src.storage.sql_storage, "get_connection", fake_get_connection)
+
+    src.db.init_db()
+
+    library = SqlLibraryStorage(test_db_path)
 
     assert library.get_stats() == {
                                     "total_books": 0,
@@ -14,7 +27,17 @@ def test_get_stats_empty_library():
                                 }
     
     
-def test_get_stats_after_adding_book():
+def test_get_stats_after_adding_book(monkeypatch, tmp_path):
+    test_db_path = tmp_path / "test.db"
+
+    def fake_get_connection():
+        return sqlite3.connect(test_db_path)
+    
+    monkeypatch.setattr(src.db, "get_connection", fake_get_connection)
+    monkeypatch.setattr(src.storage.sql_storage, "get_connection", fake_get_connection)
+
+    src.db.init_db()
+
     book = Book(
                 title="test",
                 book_id="test123GHbffdf",
@@ -25,7 +48,7 @@ def test_get_stats_after_adding_book():
                 date_published="2077-03-16",
                 isbn="FdsBHGf45DSs"
            )
-    library = SqlLibraryStorage("test_sql.db")
+    library = SqlLibraryStorage(test_db_path)
 
     library.add(book)
     stats = library.get_stats()
